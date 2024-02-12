0f9d6d7bd593dd3198a35fd428ddb7bdae74d200
Sasa537 <sasa5375632@gmail.com>
2024-02-12 18:49:52 +0300

    register new app and create urls files

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6xvm0he$wvth-9az@v^vtk5i0s$y^2*qtpkkved5&s5sl7w=2m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
