eb9af255cb9b6898d26ad72a99511bc2990d7866
Sasa537 <sasa5375632@gmail.com>
2024-03-02 20:38:35 +0300

    create html pages

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('WebBlog.urls'))
+    path('', include('WebBlog.urls')),
+    path('about', include('WebBlog.urls'))
 ]
--- WebBlog/views.py
+++ WebBlog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("<h1>Ленчык Безделенчык</h1>")
+    return render(request, 'webblog/index.html')
+
+
+def about(request):
+    return render(request, 'webblog/about.html')
