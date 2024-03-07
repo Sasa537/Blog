a276a9080cc5f5d4e62a29f6558572341bcafeba
Sasa537 <sasa5375632@gmail.com>
2024-03-07 18:25:54 +0300

    create madels

--- WebBlog/models.py
+++ WebBlog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Articles (models.Model):
+    title = models.CharField('Название', max_length=50)
+    preview = models.CharField('Предпросмотр', max_length=250)
+    full_text = models.TextField('Вся статья')
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статьи'
+        verbose_name_plural = 'Статьи'
