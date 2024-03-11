dac903907d8c8632b68dd26e7bd8cfb1a94f4f9f
Sasa537 <sasa5375632@gmail.com>
2024-03-11 21:14:11 +0300

    Install DRF

--- Blog/settings.py
+++ Blog/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'tinymce',
     'WebBlog',
+    'BlogAPI',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
