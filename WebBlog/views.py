98308bf1a881baa42fc0d288923310d0941ff488
Sasa537 <sasa5375632@gmail.com>
2024-03-11 14:49:35 +0300

    Create pagination in main page

--- WebBlog/views.py
+++ WebBlog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Articles
+from django.core.paginator import Paginator
 
 
 def index(request):
-    return render(request, 'webblog/index.html')
+    articles = Articles.objects.all()
+    paginator = Paginator(articles, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'webblog/index.html', {'page_obj': page_obj})
 
 
 def about(request):
